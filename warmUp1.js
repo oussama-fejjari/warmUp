80afeeff7a58ca030b8388087136e6cfe8e34ef8
unknown <samofejjari1998@gmail.com>
2020-03-13 12:32:02 +0100

    done

--- warmUp1.js
+++ warmUp1.js
@@ -4,3 +4,15 @@
 // 4-calculate your age in seconds.
 
 // your code is here
+function fullname(firstname,lastname){
+	return firstname+' '+lastname
+}
+function multiply(number,multiple){
+	if(number/multiple===0) {
+		return 'yes' 
+  }
+       return 'no'
+}
+function ageInSeconds(age){
+	return age*60*60*24*365
+}
